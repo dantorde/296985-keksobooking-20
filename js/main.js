65f6da0f4f6d4ef14c6143e42693c60614f853d6
Nastya <dantorde@gmail.com>
2020-07-01 17:33:24 +0300

    загрузка похожих объявлений с сервера

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
 'use strict';
 
 (function () {
-  var COUNT_ADS = 8;
-  var dataAds = window.data.createAds(COUNT_ADS);
   var mapBlock = document.querySelector('.map');
   var mainMark = document.querySelector('.map__pin--main');
+  var marksMap = document.querySelector('.map__pins');
 
   /**
+  * генерация меток на основе созданного массива объявлений
+  * @param {array} dataAds - массив объктов
+  * @return {object} объект
+  */
+  var onLoad = function (dataAds) {
+    var marksFragment = document.createDocumentFragment();
+    dataAds.forEach(function (dataAd) {
+      marksFragment.appendChild(window.mark.create(dataAd));
+    });
+    return marksMap.appendChild(marksFragment);
+  };
+
+
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+  /**
   * деактивация и активация карты, формы и фильтра
+  * @param {param} flag - флаг
   */
   var makeInactive = function (flag) {
     var adForm = document.querySelector('.ad-form');
@@ -32,7 +57,7 @@
       fieldsetFilterFormArr.forEach(function (fieldsetItem) {
         fieldsetItem.removeAttribute('disabled', 'disabled');
       });
-      window.map.generateMarks(dataAds);
+      window.backend.load(onLoad, onError);
       mainMark.removeEventListener('mousedown', onPressMainMark);
       mainMark.removeEventListener('keydown', onPressMainMark);
     }
